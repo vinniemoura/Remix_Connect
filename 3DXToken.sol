fdfbeb77c6564ba86d6b89620718b8a41a9e81e3
Remix Workspace <>
2022-08-05 13:11:00 -0400

    my commit

---
+++ 3DXToken.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.7.2/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.7.2/security/Pausable.sol";
+import "@openzeppelin/contracts@4.7.2/access/Ownable.sol";
+
+contract Paradoxx30 is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("Paradoxx30", "3DX") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
